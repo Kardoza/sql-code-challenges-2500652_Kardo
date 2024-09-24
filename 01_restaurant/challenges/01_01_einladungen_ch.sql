dcacdef12538808e27cc53ef1a17ee6818f7c677
Kardoza <145062291+Kardoza@users.noreply.github.com>
2024-09-24 09:50:12 +0000

    Test_Code

--- 01_restaurant/challenges/01_01_einladungen_ch.sql
+++ 01_restaurant/challenges/01_01_einladungen_ch.sql
@@ -3,4 +3,8 @@
 -- und die E-Mail-Adresse der Kundinnen/Kunden enthalten.
 -- Sortieren Sie die Liste alphabetisch nach den Nachnamen.
 
+Select nachname, Vorname, Email
+FROM Kunden
+Where Vorname = Descending;
+
 
